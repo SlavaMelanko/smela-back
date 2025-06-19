0b077bdbbcf436fd031256422adb5d503eff8634
Slava Melanko <slava.melanko@gmail.com>
2025-06-19 09:45:59 +0300

    Fixed route

--- src/routes/login/index.ts
+++ src/routes/login/index.ts
@@ -1,33 +1,11 @@
 import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
 
-import { createPasswordEncoder } from '@/lib/crypto'
-import jwt from '@/lib/jwt'
-import { userRepo } from '@/repositories'
-
+import loginHandler from './handler'
 import loginSchema from './schema'
 
 const loginRoute = new Hono()
 
-loginRoute.post('/login', zValidator('json', loginSchema), async (c) => {
-  const { email, password } = await c.req.json()
-
-  const user = await userRepo.findByEmail(email)
-
-  if (!user) {
-    return c.json({ error: 'Invalid credentials' }, 401)
-  }
-
-  const encoder = createPasswordEncoder()
-  const isPasswordValid = await encoder.compare(password, user.password)
-
-  if (!isPasswordValid) {
-    return c.json({ error: 'Invalid credentials' }, 401)
-  }
-
-  const token = await jwt.sign(user.id, user.email, 'user')
-
-  return c.json({ token })
-})
+loginRoute.post('/login', zValidator('json', loginSchema), loginHandler)
 
 export default loginRoute
