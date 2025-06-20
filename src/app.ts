6e5e2652e70d0e22739cb05c88d4b126fa1e37eb
Slava Melanko <slava.melanko@gmail.com>
2025-06-20 10:51:51 +0300

    Added server

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,5 @@
-import { Hono } from 'hono'
-import { cors } from 'hono/cors'
-import { requestId } from 'hono/request-id'
+import Server from './server'
 
-import type { AppContext } from '@/types/context'
+const server = new Server()
 
-import { jwtMiddleware, loggerMiddleware, onError } from '@/middleware'
-import { privateRoutes, publicRoutes } from '@/routes'
-
-const app = new Hono<AppContext>({ strict: false })
-
-app
-  .use(cors())
-  .use(requestId())
-  .use(loggerMiddleware)
-  .use('/api/v1/*', jwtMiddleware)
-
-app.onError(onError)
-
-publicRoutes.forEach((route) => {
-  app.route('/', route)
-})
-
-privateRoutes.forEach((route) => {
-  app.route('/api/v1', route)
-})
-
-export default app
+export default server.getApp()
---
+++ src/server.ts
@@ -0,0 +1,47 @@
+import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { requestId } from 'hono/request-id'
+
+import type { AppContext } from '@/types/context'
+
+import { jwtMiddleware, loggerMiddleware, onError } from '@/middleware'
+import { privateRoutes, publicRoutes } from '@/routes'
+
+class Server {
+  readonly app: Hono<AppContext>
+
+  constructor() {
+    this.app = new Hono<AppContext>({ strict: false })
+    this.setupMiddleware()
+    this.setupRoutes()
+    this.setupErrorHandler()
+  }
+
+  private setupMiddleware() {
+    this.app
+      .use(cors())
+      .use(requestId())
+      .use(loggerMiddleware)
+      .use('/api/v1/*', jwtMiddleware)
+  }
+
+  private setupRoutes() {
+    publicRoutes.forEach((route) => {
+      this.app.route('/', route)
+    })
+
+    privateRoutes.forEach((route) => {
+      this.app.route('/api/v1', route)
+    })
+  }
+
+  private setupErrorHandler() {
+    this.app.onError(onError)
+  }
+
+  getApp() {
+    return this.app
+  }
+}
+
+export default Server
