a0557bb70423acd1ea6d4abc244183d49bdfd267
Slava Melanko <slava.melanko@gmail.com>
2025-06-23 20:12:30 +0300

    Update Role enum values for Enterprise and SelfServe
    Changed the string values for Enterprise and SelfServe roles to 'enterprise' and 'self-serve' respectively to standardize naming conventions.

--- src/types/role.ts
+++ src/types/role.ts
@@ -1,8 +1,8 @@
 enum Role {
   Owner = 'owner',
   Admin = 'admin',
-  Enterprise = 'enterprise_user',
-  SelfServe = 'selfserve_user',
+  Enterprise = 'enterprise',
+  SelfServe = 'self-serve',
 }
 
 export default Role
