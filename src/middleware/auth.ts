923b045dc7fc5189999f54b947d76506d52ed4ae
Slava Melanko <slava.melanko@gmail.com>
2025-07-12 13:00:12 +0300

    Update auth.ts

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,14 +2,21 @@ import type { MiddlewareHandler } from 'hono'
 
 import { bearerAuth } from 'hono/bearer-auth'
 
+import type { Status } from '@/types'
 import type { AppContext } from '@/types/context'
 
+import { AppError, ErrorCode } from '@/lib/errors'
 import jwt from '@/lib/jwt'
+import { isActive } from '@/types'
 
 const jwtMiddleware: MiddlewareHandler<AppContext> = bearerAuth({
   verifyToken: async (token, c) => {
     try {
       const payload = await jwt.verify(token)
+
+      if (!isActive(payload.status as Status)) {
+        throw new AppError(ErrorCode.Forbidden)
+      }
 
       c.set('user', payload)
 
