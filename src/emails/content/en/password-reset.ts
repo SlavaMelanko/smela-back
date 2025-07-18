d49cf37b31c794cc5d155d93854a5920f38e7d57
Slava Melanko <slava.melanko@gmail.com>
2025-07-18 11:52:27 +0300

    Update password reset email body text
    Reworded the body of the password reset email to instruct users to click the link below to set a new password, instead of choosing a new password via a button.

--- src/emails/content/en/password-reset.ts
+++ src/emails/content/en/password-reset.ts
@@ -6,7 +6,7 @@ export const content: PasswordResetEmailContent = {
   subject: 'Reset Your Password',
   previewText: `Reset your password for ${config.company.name} account`,
   greeting: (firstName?: string) => `Hi ${firstName || 'there'} 👋`,
-  body: 'We received a request to reset your password. Click the button below to choose a new password:',
+  body: 'We received a request to reset your password. Click the link below to set a new password:',
   ctaText: 'Reset Password',
   disclaimer: 'If you didn\'t request a password reset, you can safely ignore this email.',
   expiryNotice: 'This link will expire in 1 hour for security reasons.',
