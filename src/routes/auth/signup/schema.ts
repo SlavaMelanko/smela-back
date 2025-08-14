d9f05a3315881bbf1377e6de407acbe68cab1973
Slava Melanko <slava.melanko@gmail.com>
2025-08-14 14:53:16 +0300

    Make lastName optional in signup schema
    Updated the signup schema to use the optional version of the name rule for the lastName field, allowing users to sign up without providing a last name.

--- src/routes/auth/signup/schema.ts
+++ src/routes/auth/signup/schema.ts
@@ -2,7 +2,7 @@ import { buildSchema, userRules } from '@/lib/validation'
 
 const signupSchema = buildSchema({
   firstName: userRules.name,
-  lastName: userRules.name,
+  lastName: userRules.name.opt,
   email: userRules.email,
   password: userRules.password,
 })
