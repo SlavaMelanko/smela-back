f570e3fefd0fc66388b19a44a4fdbd3b4659bbb9
Slava Melanko <slava.melanko@gmail.com>
2025-09-22 20:03:42 +0300

    Update index.ts

--- src/lib/env/index.ts
+++ src/lib/env/index.ts
@@ -1,10 +1,10 @@
 import env from './env'
 
-export { env as default }
-
 export const isDevEnv = () => env.NODE_ENV === 'development'
 export const isProdEnv = () => env.NODE_ENV === 'production'
 export const isTestEnv = () => env.NODE_ENV === 'test'
 export const isStagingEnv = () => env.NODE_ENV === 'staging'
 export const isDevOrTestEnv = () => isDevEnv() || isTestEnv()
 export const isStagingOrProdEnv = () => isStagingEnv() || isProdEnv()
+
+export default env
