4bc5bdac133fb855fbd657c75419b507b6d1b369
Slava Melanko <slava.melanko@gmail.com>
2026-01-18 22:35:40 +0200

    Add query params and route params to request logger

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,9 +1,39 @@
+import type { Context } from 'hono'
+
 import { pinoLogger } from 'hono-pino'
 
 import { logger } from '@/logging'
 
+const onReqBindings = (c: Context) => {
+  const url = new URL(c.req.url)
+  const query = Object.fromEntries(url.searchParams)
+  const params = c.req.param()
+
+  return {
+    reqId: c.get('requestId'),
+    req: {
+      url: c.req.path,
+      method: c.req.method,
+      headers: c.req.header(),
+      ...(Object.keys(query).length > 0 && { query }),
+      ...(Object.keys(params).length > 0 && { params }),
+    },
+  }
+}
+
+const onResBindings = (c: Context) => ({
+  res: {
+    status: c.res.status,
+    headers: Object.fromEntries(c.res.headers),
+  },
+})
+
 const loggerMiddleware = pinoLogger({
   pino: logger,
+  http: {
+    onReqBindings,
+    onResBindings,
+  },
 })
 
 export default loggerMiddleware
