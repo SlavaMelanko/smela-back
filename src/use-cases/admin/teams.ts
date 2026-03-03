bfa65d32cda2931eabe2c07d008364a244034bec
Slava Melanko <slava.melanko@gmail.com>
2026-03-03 21:59:20 +0200

    Move createTeam and getTeams to admin use cases

--- src/use-cases/admin/teams.ts
+++ src/use-cases/admin/teams.ts
@@ -1,2 +1,22 @@
-export { createTeam, getTeams } from '@/use-cases/user/teams'
-export type { CreateTeamParams } from '@/use-cases/user/teams'
+import type { PaginationParams, TeamSearchParams } from '@/data'
+
+import { teamRepo } from '@/data'
+
+export interface CreateTeamParams {
+  name: string
+  website?: string
+  description?: string
+}
+
+export const getTeams = async (
+  params: TeamSearchParams,
+  pagination: PaginationParams,
+) => {
+  return teamRepo.search(params, pagination)
+}
+
+export const createTeam = async (params: CreateTeamParams) => {
+  const team = await teamRepo.create(params)
+
+  return { team }
+}
